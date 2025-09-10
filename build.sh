847675b86260d19e6d84e043b5bde91ccc425b1b
Shah Tabrez <tabrez.shah@opstree.com>
2025-09-10 22:46:19 +0530

    updated files

--- build.sh
+++ build.sh
@@ -4,15 +4,27 @@ source /opt/buildpiper/shell-functions/log-functions.sh
 
 
 CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
-SLEEP_DURATION= 5
+SLEEP_DURATION=5
 logInfoMessage "I'll build the code available at [$CODEBASE_LOCATION]"
 sleep  $SLEEP_DURATION
 
-cd  "${CODEBASE_LOCATION}"
+cd "${CODEBASE_LOCATION}" || {
+  logErrorMessage "Failed to change directory to ${CODEBASE_LOCATION}"
+  exit 1
+}
+
 logInfoMessage "Running flutter pub get"
-flutter pub get
-logInfoMessage "I'll execute instruction $INSTRUCTION"
-flutter $INSTRUCTION
+if ! flutter pub get; then
+  logErrorMessage "flutter pub get failed!"
+  exit 1
+fi
+
+logInfoMessage "I'll execute instruction: flutter $INSTRUCTION"
+if ! flutter $INSTRUCTION; then
+  logErrorMessage "flutter $INSTRUCTION failed!"
+  exit 1
+fi
+
 TASK_STATUS=$?
 
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
