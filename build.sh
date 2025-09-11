3b73b67dd360e5c7618eb7f5ce890794ccbdf7fe
Shah Tabrez <tabrez.shah@opstree.com>
2025-09-11 12:25:48 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -2,30 +2,38 @@
 source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
 
-
-CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
+CODEBASE_LOCATION="${WORKSPACE}/${CODEBASE_DIR}"
 SLEEP_DURATION=5
-logInfoMessage "I'll build the code available at [$CODEBASE_LOCATION]"
-sleep  $SLEEP_DURATION
 TASK_STATUS=0
 
-cd "${CODEBASE_LOCATION}" || {
+logInfoMessage "I'll build the code available at [$CODEBASE_LOCATION]"
+sleep $SLEEP_DURATION
+
+# Step 1: change directory
+if ! cd "${CODEBASE_LOCATION}"; then
   logErrorMessage "Failed to change directory to ${CODEBASE_LOCATION}"
   TASK_STATUS=1
-}
-
-logInfoMessage "Running flutter pub get"
-if ! flutter pub get; then
-  logErrorMessage "flutter pub get failed!"
-  TASK_STATUS=1
+  saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
 fi
 
-logInfoMessage "I'll execute instruction: flutter $INSTRUCTION"
-if ! flutter $INSTRUCTION; then
-  logErrorMessage "flutter $INSTRUCTION failed!"
-  TASK_STATUS=1
+# Step 2: flutter pub get
+if [[ $TASK_STATUS -eq 0 ]]; then
+  logInfoMessage "Running flutter pub get"
+  if ! flutter pub get; then
+    logErrorMessage "flutter pub get failed!"
+    TASK_STATUS=1
+    saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
+  fi
 fi
 
-TASK_STATUS=$?
+# Step 3: flutter instruction
+if [[ $TASK_STATUS -eq 0 ]]; then
+  logInfoMessage "I'll execute instruction: flutter $INSTRUCTION"
+  if ! flutter $INSTRUCTION; then
+    logErrorMessage "flutter $INSTRUCTION failed!"
+    TASK_STATUS=1
+  fi
+fi
 
+# Final save — will decide exit or not based on VALIDATION_FAILURE_ACTION
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
