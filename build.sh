3f6fa73d482380ec94cadaa80bf10683202ad17a
Shah Tabrez <tabrez.shah@opstree.com>
2025-09-11 11:03:02 +0530

    updated build.sh

--- build.sh
+++ build.sh
@@ -7,22 +7,23 @@ CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
 SLEEP_DURATION=5
 logInfoMessage "I'll build the code available at [$CODEBASE_LOCATION]"
 sleep  $SLEEP_DURATION
+TASK_STATUS=0
 
 cd "${CODEBASE_LOCATION}" || {
   logErrorMessage "Failed to change directory to ${CODEBASE_LOCATION}"
-  exit 1
+  TASK_STATUS=1
 }
 
 logInfoMessage "Running flutter pub get"
 if ! flutter pub get; then
   logErrorMessage "flutter pub get failed!"
-  exit 1
+  TASK_STATUS=1
 fi
 
 logInfoMessage "I'll execute instruction: flutter $INSTRUCTION"
 if ! flutter $INSTRUCTION; then
   logErrorMessage "flutter $INSTRUCTION failed!"
-  exit 1
+  TASK_STATUS=1
 fi
 
 TASK_STATUS=$?
